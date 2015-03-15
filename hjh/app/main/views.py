2a2d4493c0499ef7896dc2fad34cb60b4d7865cd
jiho <polaris341@gmail.com>
2015-03-15 11:36:58 +0900

    3월 15일 진행

--- hjh/app/main/views.py
+++ hjh/app/main/views.py
@@ -4,3 +4,7 @@ from flask import render_template
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/test')
+def test():
+    return render_template('test.html')
